00fca1d046052b9d65248823cf4ed3913e609b75
Diki Alfarabi Hadi <malasngoding@gmail.com>
2018-09-18 16:34:50 +0700

    file

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MEMBUAT GRAFIK DARI DATABASE MYSQL DENGAN PHP DAN CHART.JS - www.malasngoding.com</title>
+	<script type="text/javascript" src="chartjs/Chart.js"></script>
+</head>
+<body>
+	<style type="text/css">
+	body{
+		font-family: roboto;
+	}
+
+	table{
+		margin: 0px auto;
+	}
+	</style>
+
+
+	<center>
+		<h2>MEMBUAT GRAFIK DARI DATABASE MYSQL DENGAN PHP DAN CHART.JS<br/>- www.malasngoding.com -</h2>
+	</center>
+
+
+	<?php 
+	include 'koneksi.php';
+	?>
+
+	<div style="width: 800px;margin: 0px auto;">
+		<canvas id="myChart"></canvas>
+	</div>
+
+	<br/>
+	<br/>
+
+	<table border="1">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Nama Mahasiswa</th>
+				<th>NIM</th>
+				<th>JK</th>
+				<th>Fakultas</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+			$no = 1;
+			$data = mysqli_query($koneksi,"select * from mahasiswa");
+			while($d=mysqli_fetch_array($data)){
+				?>
+				<tr>
+					<td><?php echo $no++; ?></td>
+					<td><?php echo $d['nama']; ?></td>
+					<td><?php echo $d['nim']; ?></td>
+					<td align="center" bgcolor="pink"><?php echo $d['jenis_kelamin']; ?></td>
+					<td><?php echo $d['fakultas']; ?></td>
+				</tr>
+				<?php 
+			}
+			?>
+		</tbody>
+	</table>
+
+
+	<script>
+		var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["Laki-laki", "Perempuan"],
+				datasets: [{
+					label: '',
+					data: [
+					<?php 
+					$jumlah_laki = mysqli_query($koneksi,"select * from mahasiswa where jenis_kelamin='L'");
+					echo mysqli_num_rows($jumlah_laki);
+					?>, 
+					<?php 
+					$jumlah_perempuan = mysqli_query($koneksi,"select * from mahasiswa where jenis_kelamin='P'");
+					echo mysqli_num_rows($jumlah_perempuan);
+					?>
+					],
+					backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)'
+					],
+					borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+	</script>
+</body>
+</html>
